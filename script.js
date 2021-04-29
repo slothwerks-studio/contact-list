0f8a4466a31f67c0cc4e514aef8691efa38d4b10
hainesma <hainesma@gmail.com>
2021-04-28 22:26:16 -0400

    remove trim from this branch

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function handleFormSubmit(event) {
   event.preventDefault();
   // Check for user input; if no input, display alert
   // Otherwise move forward with contact processing
-  if (nameInput.value.trim() && phoneInput.value.trim()) {
+  if (nameInput.value && phoneInput.value) {
     // Process contact based on mode
     if (formMode === "Add") {
       // Build new contact object
