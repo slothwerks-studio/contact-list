87219244f333cc904619f6a4bfa2c089b7099e82
Sloth I <github@slothwerks.com>
2021-08-21 15:19:11 -0400

    Added handlers for contact items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import { retrieveLocalStorage } from './storage';
+import { retrieveLocalStorage, updateLocalStorage } from './storage';
 
 function App() {
 
   // Set initial state
   const [contacts, setContacts] = useState([]);
-  const [formMode, setFormMode] = useState("Add");
   const [contactToEdit, setContactToEdit] = useState(null);
+  const [form, setForm] = useState({
+    mode: "Add",
+    name: "",
+    phone: ""
+  })
 
   // On load, retrieve contacts from storage
   useEffect(() => {
@@ -14,13 +18,77 @@ function App() {
     setContacts(contactsInStorage);
   }, []);
 
+  // Build handlers for contact cards
+  function handleUpdateContact(id) {
+    // Find contact in contact list
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const contact = contacts.find(function (contact) {
+      // Return the contact with the ID matching the selected contact ID
+      return contact.id === id;
+    });
+    // Update contactToEdit with contact data
+    setContactToEdit(contact);
+    // Update form values
+    setForm({
+      mode: "Update",
+      name: contact.name,
+      phone: contact.phone
+    });
+  }
+
+  function handleRemoveContact(id) {
+    // Find contact in contact list
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+    const contact = contacts.find(function (contact) {
+      // Return the contact with the ID matching the selected contact ID
+      return contact.id === id;
+    });
+    if (window.confirm(`Are you sure you want to remove ${contact.name} from your contact list?`)) {
+      // Build new contact list using filter
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+      const newContacts = contacts.filter(function (contact) {
+        // If the contact ID does *not* match the selected ID,
+        // add it to a new contacts array
+        return contact.id !== id;
+      });
+      // Update the contacts array
+      setContacts(newContacts);
+      // Update localStorage with changes
+      updateLocalStorage(newContacts);
+    }
+  }
+
+  // Use map to go through the contacts array and return contact cards for the UI
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+  const contactList = contacts.map(function (contact) {
+    return (
+      <div className="contact">
+        <a className="contact-link" href={`tel:+1${contact.phone}`}>{contact.name}</a>
+          <button 
+            type="button"
+            className="contact-option-button"
+            onclick={() => handleUpdateContact(contact.id)}
+          >
+            [update]
+          </button>
+          <button 
+            type="button"
+            className="contact-option-button"
+            onclick={() => handleRemoveContact(contact.id)}
+          >
+            [remove]
+        </button>
+      </div>
+    );
+  });
+
   return (
     <Fragment>
       <header>
         <h1>
           My Contacts
         </h1>
-        <p class="version-number">
+        <p className="version-number">
           ver. 0.3.3
         </p>
       </header>
@@ -34,7 +102,7 @@ function App() {
           </h2>
 
           <form id="contact-form">
-            <div class="input-wrapper">
+            <div className="input-wrapper">
               <label for="name">
                 Name
               </label>
@@ -46,7 +114,7 @@ function App() {
                 maxlength="50"
               />
             </div>
-            <div class="input-wrapper">
+            <div className="input-wrapper">
               <label for="phone">
                 Phone
               </label>
@@ -58,7 +126,7 @@ function App() {
                 pattern="[0-9]{10}"
               />
             </div>
-            <div class="button-wrapper">
+            <div className="button-wrapper">
               <button 
                 id="submit-button" 
                 type="submit"
@@ -83,7 +151,7 @@ function App() {
           </h2>
 
           <div id="contact-list">
-            {/* Contacts will go here */}
+            { contactList }
           </div>
 
         </section>
